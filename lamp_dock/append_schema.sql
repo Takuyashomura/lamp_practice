4658b242943196b8ee0c7e1116a16d8f25a33088
Takuya Shomura <takudai1411@yahoo.co.jp>
2019-11-10 21:31:37 +0900

    一部カラムを変更

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -106,7 +106,7 @@ CREATE TABLE `purchase_history` (
 
 CREATE TABLE `purchase_datails`(
   `details_id`  int(11) NOT NULL,
-  `user_id`     int(11) NOT NULL,
+  `order_id`     int(11) NOT NULL,
   `item_id`     int(11) NOT NULL,
   `purchase_item_price`   int(11) NOT NULL,
   `purchase_item_amount`  int(11) NOT NULL
